ccac31d08d6a15b9fd80e703a953e7c7d8a166f4
Cristian Brinza <cristian.brinza@isa.utm.md>
2023-06-11 23:35:28 +0300

    update

--- interpreter.py
+++ interpreter.py
@@ -9,5 +9,27 @@ class Interpreter:
 
     def interpret(self):
         ast = self.parser.parse()
-        for key, value in ast.items():
-            print(f"{key}: {value}")
+        cbc_params = ['WBC', 'RBC', 'Hemoglobin']
+        reference_range = {
+            'WBC': (4.0, 11.0),
+            'RBC': (4.5, 5.5),
+            'Hemoglobin': (12.0, 16.0)
+        }
+
+        result = {}
+        for param in cbc_params:
+            value = ast.get(param)
+            if value is not None:
+                result[param] = value
+
+        comments = "Normal CBC results"
+        
+        print("result:")
+        for param, value in result.items():
+            print(f"  - {param}: {value}")
+        
+        print("reference_range:")
+        for param, (lower, upper) in reference_range.items():
+            print(f"  - {param}: {lower} - {upper}")
+        
+        print(f"comments: {comments}")
